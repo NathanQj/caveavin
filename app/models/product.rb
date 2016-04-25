98d4f1dd16c1c8460c4d7220c5c2c278761dab60
NathanQ <nathan.qjm@gmail.com>
2016-04-25 16:19:25 +0200

    model: varietal

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,12 @@ class Product < ActiveRecord::Base
   has_one :appellation
   has_one :varietal
   has_one :vineyard
+  has_one :retail
+  has_one :community
+
   has_many :product_attributes
   has_many :labels
   has_many :ratings
-  has_one :retail
   has_many :vintages
-  has_one :community
 
 end
